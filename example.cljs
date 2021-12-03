a8f61eebb418bf4ca143737f5dd3d78534a0a83f
porkostomus <porkostomus@mail.com>
2021-12-03 13:21:05 -0800

    use aplay

--- example.cljs
+++ example.cljs
@@ -1,35 +1,14 @@
 (ns example
-  (:require
-   ["fs" :as fs]
-   ["speaker$default" :refer [Speaker]]
-   ["web-audio-api$default" :as wa]))
+  (:require ["fs" :as fs]
+            ["web-audio-api$default" :as wa]))
 
 (def ^:dynamic context (wa/AudioContext.))
-
-(def Speaker (require 
-(set!
-  (.-outStream context)
-  (new
-    Speaker))
-
-
-
-(set! (.-outStream context) 
-      (Speaker. #js{:channels (.. context -format -numberOfChannels)
-     :bitDepth (.. context -format -bitDepth)
-     :sampleRate (.-sampleRate context)}))
-
-(macroexpand '(Speaker. #js {:channels (.. context -format -numberOfChannels),
-                     :bitDepth (.. context -format -bitDepth),
-                     :sampleRate (.-sampleRate context)}))
-
+(set! (.-outStream context) (.-stdout js/process))
 
 (fs/readFile "outfoxing.mp3"
       (fn [err buffer]
         (when err (throw err))
-        (.decodeAudioData
-          context
-          buffer
+        (.decodeAudioData context buffer
           (fn [audioBuffer]
             (let [bufferNode (.createBufferSource context)]
               (.connect bufferNode (.-destination context))
